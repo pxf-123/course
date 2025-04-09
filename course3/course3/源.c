4b42461919536ceb4b42f6aeb15ecc61074e405b
pxf-123 <1823208497@qq.com>
2025-04-09 20:51:26 +0800

    Add files via upload

---
+++ course3/course3/源.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("%zu",sizeof(char));
+	printf("%zu", sizeof(short));
+	printf("%zu", sizeof(int));
+	printf("%zu", sizeof(long));
+	printf("%zu",sizeof(long long));
+	printf("%zu", sizeof(float));
+	printf("%zu", sizeof(double));
+	return 0;
+}
